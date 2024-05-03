9dfff7e9983103ff1136cfca221e30ab1394e1aa
Franco <francobetteo@gmail.com>
2024-05-03 12:37:48 -0300

    typo in origin

--- src/fastapi_main.py
+++ src/fastapi_main.py
@@ -60,7 +60,7 @@ print(ALGORITHM)
 # I think this was needed to allow the frontend to connect to the backend
 origins = [
     "https://youtube-channel-to-gpt-frontend-r1kgr3mb0.vercel.app",
-    "https://youtube-channel-to-gpt-frontend.vercel.app/",
+    "https://youtube-channel-to-gpt-frontend.vercel.app",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "localhost:3000",
