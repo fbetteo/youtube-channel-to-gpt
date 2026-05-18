e04d38e495fe14f82f1c86323c73d96718901bd6
fbetteo <francobetteo@gmail.com>
2026-05-18 10:58:51 -0300

    add prod alias to lambda invoke

--- src/youtube_service.py
+++ src/youtube_service.py
@@ -2124,6 +2124,7 @@ async def dispatch_lambdas_concurrently(
             await asyncio.to_thread(
                 lambda_client.invoke,
                 FunctionName=f"{settings.lambda_function_name}",
+                Qualifier="prod",
                 InvocationType="Event",  # Asynchronous invocation
                 Payload=json.dumps(lambda_payload),
             )
